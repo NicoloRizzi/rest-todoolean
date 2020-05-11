078fcd42beeacea4d6eb339dc050786efa5adccc
Nicolo <nicolorizzi1996@gmail.com>
2020-05-11 17:23:33 +0200

    ADDED FUNCTION DELETE TODO

--- js/main.js
+++ js/main.js
@@ -5,36 +5,22 @@ $(document).ready(function () {
 
     // ref
     var newInputTodo = $('.new-todo');
-    var btnSubmitTodo = $('submit-new-todo');
+    var newTodoBtn = $('.submit-new-todo');
     var listTodo = $('.todolist');
     var apiUrl = 'http://157.230.17.132:3017/todos/';
-    var newTodoBtn = $('.submit-new-todo');
 
     // GET TODOS
     printTodos(apiUrl, template, listTodo)
 
     // create a new todo Item
     newTodoBtn.click(function(){
-        var todoValue = newInputTodo.val().trim();
-        $.ajax({
-            type:"POST",
-            url: apiUrl,
-            data: {
-                text: todoValue
-            },
-            success: function() {
-                printTodos(apiUrl, template, listTodo);
-
-            },
-            error: function (){
-                console.error('Errore nella creazione del nuovo Todo');
-            }
-        })
+        createNewTodo(apiUrl, newInputTodo, template, listTodo);
     });
 
     // REMOVE TODO ITEM
     $(document).on('click','.remove', function(){
-        var todoId = $(this).data('id');
+        deleteTodo($(this), apiUrl, template, listTodo)
+    /* var todoId = $(this).data('id');
         $.ajax({
             type:"DELETE",
             url: apiUrl + todoId,
@@ -45,15 +31,15 @@ $(document).ready(function () {
                 console.error('Errore durante la cancellazione del todo');
             }
 
-        })
-    })
+        })*/
+    });
 
 }); //------ END DOC READY
 
 /**
  * FUNCTIONS
  */
-function printTodos(API,template, list){
+function printTodos(API, template, list){
     // clean html
     list.html('');
     $.ajax({
@@ -74,4 +60,37 @@ function printTodos(API,template, list){
             console.error('Errore nella richiesta dei todos')
         }
     });
+}
+// create a new todo
+function createNewTodo(API, input, template, list) {
+    var todoValue = input.val().trim();
+    $.ajax({
+        type: "POST",
+        url: API,
+        data: {
+            text: todoValue
+        },
+        success: function () {
+            printTodos(API, template, list);
+        },
+        error: function () {
+            console.error('Errore nella creazione del nuovo Todo');
+        }
+    })
+}
+
+// delete a todo
+function deleteTodo(self,API, template, list){
+    var todoId = self.data('id');
+    $.ajax({
+        type: "DELETE",
+        url: API + todoId,
+        success: function () {
+            printTodos(API, template, list);
+        },
+        error: function () {
+            console.error('Errore durante la cancellazione del todo');
+        }
+
+    })
 }
