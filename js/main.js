42ff33bd58b26f6c074a53a5bc121e70fd6daf7d
Nicolo <nicolorizzi1996@gmail.com>
2020-05-11 16:14:46 +0200

    FUNCTION READ TODO FROM API

--- js/main.js
+++ js/main.js
@@ -8,8 +8,34 @@ $(document).ready(function () {
     var btnSubmitTodo = $('submit-new-todo');
     var listTodo = $('.todolist');
     var apiUrl = 'http://157.230.17.132:3017/todos/';
+
+    // GET TODOS
+    printTodos(apiUrl, template, listTodo)
+
 }); //------ END DOC READY
 
 /**
  * FUNCTIONS
  */
+function printTodos(API,template, list){
+    // clean html
+    list.html('');
+    $.ajax({
+        type: "GET",
+        url: API,
+        success: function (data) {
+            for(var i = 0; i < data.length; i++) {
+                var todo = data[i];
+                var context = {
+                    todo: todo.text,
+                    id: todo.id
+                }
+                var html = template(context);
+                list.append(html);
+            }
+        },
+        error: function () {
+            console.error('Errore nella richiesta dei todos')
+        }
+    });
+}
